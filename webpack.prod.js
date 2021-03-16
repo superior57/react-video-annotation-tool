386cb0a3803eb49ce136fef8a2586083559da7da
talentedexpert0057 <petpanda0057@gmail.com>
2021-03-17 02:24:58 +1000

    updated webpack for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
 	output: {
 		path: path.resolve(__dirname, 'dist/'),
 		publicPath: '/dist/',
-		filename: 'bundle.js',
+		filename: 'demo.bundle.js',
 		libraryTarget: 'commonjs2',
 	},
 	plugins: [
